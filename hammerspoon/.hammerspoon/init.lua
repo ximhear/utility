ea86aabbc0d7182430b68dbfb10c01dd26ca384b
C.H Lee <ximhear@gmail.com>
2026-06-15 12:35:23 +0900

    Update init.lua

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,3 +1,5 @@
+-- Escape 키를 누르면 입력 소스를 영어(ABC)로 전환
+-- 전역 변수로 두어 eventtap이 GC되지 않도록 함
 escapeTap = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
 	if event:getKeyCode() == hs.keycodes.map["escape"] then
 		hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
